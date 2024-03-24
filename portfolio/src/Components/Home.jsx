0760b2dc30eec9cf40c09c1729711d5170546765
adavid2021 <ajdavid@bu.edu>
2024-03-24 18:26:47 -0400

    added contact page and links

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -1,20 +1,12 @@
-// import { useState } from 'react'
-import NavBar from "./NavBar.jsx";
-
 function Home() {
-    // const [count, setCount] = useState(0)
-
     return (
         <>
             <div className="home-page global-style">
                 <div className="opaque-bg">
-                    <div className="home-name">
-                        <h1 id="home-text" style={{opacity: 100}}>Aidan David</h1>
-                        <h3 id="home-text2">Data Analyst / Software Engineer</h3>
+                    <div className="center-text-box">
+                        <h1>Aidan David</h1>
+                        <h3>Data Analyst / Software Engineer</h3>
                     </div>
-                    {/*<h2>this is an H2</h2>*/}
-                    {/*<h3>this is an H3</h3>*/}
-                    {/*<h4>this is an H4</h4>*/}
                 </div>
             </div>
         </>
--- portfolio/src/Components/Portfolio.jsx
+++ portfolio/src/Components/Portfolio.jsx
@@ -1,9 +1,4 @@
-// import { useState } from 'react'
-import NavBar from "./NavBar.jsx";
-
 function Portfolio() {
-    // const [count, setCount] = useState(0)
-
     return (
         <>
             <div className="portfolio-page global-style">
