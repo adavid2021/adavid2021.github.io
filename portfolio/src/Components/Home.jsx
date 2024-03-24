d1b7ae5c6f9748928c655b9f4159dd55c587082c
adavid2021 <ajdavid@bu.edu>
2024-03-24 16:57:00 -0400

    refined home page and nav-bar

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -7,7 +7,15 @@ function Home() {
     return (
         <>
             <div className="home-page global-style">
-                home
+                <div className="opaque-bg">
+                    <div className="home-name">
+                        <h1 id="home-text" style={{opacity: 100}}>Aidan David</h1>
+                        <h3 id="home-text2">Data Analyst / Software Engineer</h3>
+                    </div>
+                    {/*<h2>this is an H2</h2>*/}
+                    {/*<h3>this is an H3</h3>*/}
+                    {/*<h4>this is an H4</h4>*/}
+                </div>
             </div>
         </>
     )
