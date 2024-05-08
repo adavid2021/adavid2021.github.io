4a88e3dc19c9536fd4cb964e223a89430a2ab0e1
adavid2021 <ajdavid@bu.edu>
2024-05-08 13:26:53 -0400

    edited text on main pages

--- src/Components/ProjectNBATwitter.jsx
+++ src/Components/ProjectNBATwitter.jsx
@@ -43,20 +43,21 @@ function ProjectNBATwitter() {
                                     <b>Technical:</b>
                                     <br/>
                                     <p>
-                                        I learned how to use an API to obtain large quantities of social media data. I
-                                        used R scripts to automate data processing of this text data. Also, I also
-                                        generated graphs of to connect network data by ID.
+                                        I learned how to use an API to obtain large quantities of social media data and
+                                        then cleaned the data with R scripts. I also
+                                        generated graphs connecting Twitter user data by ID.
                                     </p>
                                 </div>
                                 <div className="border-right"/>
                                 <div className="project-upper-band-item lower-padding">
                                     <h4>Project Findings</h4>
                                     <p>
-                                        Interpreting results of NLP is not always straightforward and requires an
-                                        analyst to have some contextual knowledge of the subject matter. For this
-                                        project, I follow the NBA making understanding and analyzing its social media
-                                        data easier. However, I am not as familiar with college football which made me
-                                        look into the event the data was selected from.
+                                        Interpreting results of NLP is not always straightforward and requires
+                                        contextual knowledge to understand trends. I follow the NBA which made
+                                        understanding and analyzing its social media
+                                        data easier. However, I needed to research the college football game to
+                                        understand the graph of the second part of the project as I am less familiar
+                                        with college football.
                                     </p>
                                 </div>
                             </div>
@@ -85,7 +86,8 @@ function ProjectNBATwitter() {
                                         Tatum</strong></li>
                                 </ul>
                                 <p>
-                                    Using this data connected by following ID, I generated a graph where each gray
+                                    I connected users by following ID and generated a graph (shown below) where each
+                                    gray
                                     circle is a Twitter user with a mutual following status with one of
                                     the four central players:
                                 </p>
@@ -101,23 +103,25 @@ function ProjectNBATwitter() {
                                 <p>
                                     Follower connections on Twitter seem to originate from Stephen Curry which makes
                                     sense as
-                                    he is a non-controversial NBA player with many die-hard fans and acquaintances
-                                    from his league. Players from other countries have fewer connections as Twitter
+                                    he is an NBA player with little controversy and many acquaintances and die-hard
+                                    fans. Players from other countries, like Giannis, have fewer connections as Twitter
                                     is a platform mostly used in the United States. Also, players from well
-                                    established and historically dominant franchises (like Lebron James and Jayson
-                                    Tatum from the Lakers and the Celtics) have less connections overall.
+                                    established and historically dominant franchises, like Lebron James and Jayson
+                                    Tatum, have less connections overall.
                                 </p>
                                 <hr/>
                                 <h5>Finding Associated Words in a Text Corpus</h5>
-                                <p>In this section of my project, I looked at tweets from a NCAA football game between
+                                <p>
+                                    In this section of my project, I looked at tweets from a NCAA football game between
                                     the Alabama Red Tide and the Tennessee Volunteers. Before I was able to look at a
                                     graph of the words used in these tweets I processed the whole body of text (the
                                     corpus) into lower case letters, removed punctuation, special symbols, new line
                                     tags, links and ensured the tweets were in english for easier interpretation.
                                     Then I lemmatized the text (essentially categorizing words with the root word) to
                                     reduce redundancy in token words. Last, I collected tokens and groups of two words
-                                    together (bigrams). Using the frequencies of the bigrams, I made the graph
-                                    below representing bigrams of more than one frequency:</p>
+                                    together or bigrams. Using the frequencies of the bigrams, I made the graph
+                                    below representing bigrams which were in more than one tweet:
+                                </p>
                                 <br/>
                                 <div className="img-container-nba">
                                     <img src="./ncaa_graph.png" onClick={changeNCAAShowing}
@@ -126,14 +130,16 @@ function ProjectNBATwitter() {
                                 </div>
                                 <br/>
                                 <br/>
-                                <p>This graph has two main sub-graphs, one of which has "t" at its center and
+                                <p>
+                                    This graph has two main sub-graphs, one of which has "t" at its center and
                                     another with "football" at its center. The first graph portion occurred because of
                                     contractions like "don't" and "didn't" which were separated into their own words
                                     because I removed apostrophes. The next graph portion has more meaning to it and
                                     represents the main content of the tweets. Common bigrams (with their frequency
-                                    correlating to the thickness between the connecting lines of the two words)
-                                    include "game-day", "big-orange" (which I learned was a nickname for the tennessee
-                                    team) and "saturday-3" (which must have been the date of the game).</p>
+                                    represented by the thickness between the connecting lines of the two words)
+                                    include "game-day", "big-orange" which I learned was a nickname for the tennessee
+                                    team and "saturday-3" which was the date of the game.
+                                </p>
                             </div>
                         </div>
                     </div>
